1edc805cf1323dab6d1525e41deecbd1d4bfe639
Philipe Sena Saviott <psaviott@hotmail.com>
2019-06-14 12:50:21 -0300

    .

---
+++ models.py
@@ -0,0 +1,62 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+import random
+import string
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import TimedJSONWebSignatureSerializer as BadSignature
+from itsdangerous import TimedJSONWebSignatureSerializer as SignatureExpired
+Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(1000), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'category_id': self.category_id,
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
+
+
+engine = create_engine('postgresql://catalog:bill2012@localhost/catalog')
+
+Base.metadata.create_all(engine)
